e794ac9d9d469e57fbd3ffbe06400a9762b9b9b6
Saitob <tobias_andersson1@hotmail.com>
2017-05-16 22:48:16 +0200

    Added test case for user story 2

--- src/Tests.java
+++ src/Tests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 
 public class Tests {
 
+    Bowling yesWeBowl = new Bowling();
+
     /* User story 1
     ** Motivation: Not really needed because you can't write the code any simpler.
     */
@@ -14,13 +16,32 @@ public class Tests {
     @Test
     public void testCaseUno(){
 
-        Bowling yesWeBowl = new Bowling();
+
         
         Assertions.assertTrue(yesWeBowl.makeThrow() instanceof int[]);
     }
+    
+    /* User story 2
+    */
+    // Checks if calculateFrameScore calculates the frame score correctly.
+    @Test
+    public void testCaseTwo() {
 
-    // User story 2
+        int testArr1[] = {1,2};
+        int testArr2[] = {10,0};
+        int testArr3[] = {0,0};
+        int testArr4[] = {-1,-7};
+        int testArr5[] = {11,11};
 
+        int resultArr[] = {3, 10, 0, -8, 22};
+
+        Assertions.assertEquals(calculateFrameScore(testArr1), resultArr[0]);
+        Assertions.assertEquals(calculateFrameScore(testArr2), resultArr[1]);
+        Assertions.assertEquals(calculateFrameScore(testArr3), resultArr[2]);
+        Assertions.assertEquals(calculateFrameScore(testArr4), resultArr[3]);
+        Assertions.assertEquals(calculateFrameScore(testArr5), resultArr[4]);
+
+    }
 
 }
 
